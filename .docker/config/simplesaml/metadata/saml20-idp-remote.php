f24b8e9c5b7ccebbdeccd2fb344cca663ce35c00
Roman Barbun <romanbarbun@gmail.com>
2024-11-01 17:59:04 +1100

    Simplified bindings params.

--- .docker/config/simplesaml/metadata/saml20-idp-remote.php
+++ .docker/config/simplesaml/metadata/saml20-idp-remote.php
@@ -12,43 +12,43 @@ $metadata[$idpEntityId] = [
   'SingleSignOnService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SIGNON_HTTP_POST_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_POST_BINDING') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SIGNON_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SIGNON_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SIGNON_HTTP_ARTIFACT') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_ARTIFACT') ?: $fallbackBinding),
     ],
   ],
   'SingleLogoutService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_LOGOUT_HTTP_POST_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_POST_BINDING') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_LOGOUT_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_LOGOUT_HTTP_ARTIFACT') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_ARTIFACT') ?: $fallbackBinding),
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_LOGOUT_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
     ],
   ],
   'ArtifactResolutionService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_ARTIFACT_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
       'index' => 0,
     ],
   ],
